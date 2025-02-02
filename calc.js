363cb7b684abf1e1c5652da33402f281a49ca205
Dylan M <dmurayama93@gmail.com>
2025-02-01 14:06:53 -1000

    creates basic html layout for calculator

---
+++ calc.js
@@ -0,0 +1,40 @@
+let total = 0;
+let newNumber = 0;
+const numbArray = [];
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach((btn) => {
+    btn.addEventListener('click', function(e){
+        e.target.style.background = 'black';
+    });
+});
+
+function add(number){
+    //adds number to total
+}
+
+function subtract(number){
+    //subtracts number from total
+}
+
+function multiply(number){
+    //multiplies total by number
+}
+
+function divide(number){
+    //divides total by number
+}
+
+function clear(total){
+    //clears total
+    total = 0;
+}
+
+
+// if(numbArray.length == 0){
+//     console.log(numbArray.length);
+//     numbArray.push(number)
+// }
+// else{
+    
+// }
